e4169acbb8d643294be3fb5ba0a7b70aa28bd9a5
Golu_singh <angelacarol12f@gmail.com>
2024-09-22 12:05:10 +0530

    c

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,7 +1,7 @@
 const Config = {
 
-     BASE_URL: 'http://localhost:5000',
-        //  BASE_URL: "https://camsharp-server.vercel.app", 
+    //   BASE_URL: 'http://localhost:5000',
+         BASE_URL: "https://camsharp-server02.vercel.app", 
  };
  
  export default Config;
